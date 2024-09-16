e3ab62e3a5ca2733b3b0c993124c511f3be46711
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-09-16 11:50:02 +0200

    0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,36 +1,15 @@
 #!/usr/bin/python3
 """
-function that calculates the minimum number of operations needed to achieve
-exactly n "H" characters in a file.
+Function that calculates the min operations to copy and paste letters
 """
 
 
-def minop(n):
-    """
-    Method that calculates the minimum number of operations required to get
-    exactly `n` "H" characters.
-
-    Args:
-    n (int): The target number of "H" characters.
-
-    Returns:The minimum number of operations required to get `n` "H" characters.
-    """
-    div = 2  
-    operations = 0  
-
-    if n <= 0:
-        return 0
-    else:
-        while n > 1:
-            while n % div == 0:
-                operations += div
-                n //= div
-            div += 1
-        return operations
-
-
-if __name__ == "__main__":
-    n = int(input("Enter the number of H characters you want: "))
-
-    result = minop(n)
-    print(f"Min number of operations to reach {n} characters: {result}")
+def minOperations(n):
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe

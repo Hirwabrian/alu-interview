446c01be91d18e9e922dd6c4bfcbfae0122787a9
Hirwabrian <H.brian@alustudent.com>
2024-09-29 12:44:03 +0200

    rain file

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,15 +3,18 @@
 def rain(walls):
     """Function to return the maximum water that can be stored"""
     water = 0
-    for i in range(1, len(walls)-1):
-        left = walls[i]
-        for j in range(i):
-            left = max(left, walls[j])
+    if len(walls) == 0:
+        return water
+    else:
+        for i in range(1, len(walls)-1):
+            left = walls[i]
+            for j in range(i):
+                left = max(left, walls[j])
 
-        right = walls[i]
-        for j in range(i+1, len(walls)):
-            right = max(right, walls[j])
+            right = walls[i]
+            for j in range(i+1, len(walls)):
+                right = max(right, walls[j])
 
-        water += min(left, right) - walls[i]
+            water += min(left, right) - walls[i]
     
     return water
